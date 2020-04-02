7be01bf4424d76b6567149258c782380d0867949
Han <tranthithuhan@gmail.com>
2020-04-02 21:11:04 +0200

    add board

---
+++ src/components/Board.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="board" class="board-card">
+    <h2 class="board-title">{{board.title}}</h2>
+    <b-card-group
+      deck
+      class="board-list d-flex flex-row justify-content-center"
+    >
+      <b-card
+        class="board-item"
+        v-for="item in board.list"
+        v-bind:header="item.title"
+        v-bind:key="item.id"
+      >
+        <b-list-group
+          v-for="card in item.cards"
+          v-bind:key="card.id">
+          <b-card class="board-list-card">
+            {{card.title}}
+          </b-card>
+        </b-list-group>
+
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex';
+import BoardCard from './BoardCard';
+
+export default {
+  name: 'Board',
+  components: { BoardCard },
+  props: {
+    id: String,
+  },
+  computed: {
+    board() {
+      return this.$store.getters.getBoardById(this.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .board-item {
+    max-width: 30%;
+  }
+
+  .board-list-card {
+    margin: 5px 0;
+  }
+
+  .board-list-card .card-body {
+    padding: 5px;
+  }
+</style>
---
+++ src/components/BoardCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="board-card mb-4"
+    style="max-width: calc(100%/3);">
+    <router-link
+      v-bind:to="{name: 'board', params: {id} }">
+      <b-card
+        v-bind:title="board.title"
+        v-bind:img-src="board.img"
+        img-top
+      >
+        <b-card-text>
+          Some quick example text to build on the card title.
+        </b-card-text>
+      </b-card>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoardCard',
+  props: ['board', 'id'],
+};
+</script>
+
+<style scoped>
+
+</style>
